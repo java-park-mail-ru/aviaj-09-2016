b0783d593d95704075bfae834a10baf442530150
TimeEscaper <rentgeny05@gmail.com>
2016-09-17 14:00:59 +0300

    Создана структура проекта, добавлен файл gitignore.

---
+++ src/main/java/ru/aviaj/Application.java
@@ -0,0 +1,15 @@
+package ru.aviaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sibirsky on 17.09.16.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
