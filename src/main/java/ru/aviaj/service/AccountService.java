80931b69f3339e65080f5714bc4327770eeaffd5
TimeEscaper <rentgeny05@gmail.com>
2016-09-25 15:20:48 +0300

    Добавлен класс AccountService

---
+++ src/main/java/ru/aviaj/service/AccountService.java
@@ -0,0 +1,27 @@
+package ru.aviaj.service;
+
+import org.springframework.stereotype.Service;
+import ru.aviaj.model.UserProfile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by sibirsky on 25.09.16.
+ */
+
+@Service
+public class AccountService {
+
+    private Map<String, UserProfile> loginToUser = new HashMap<String, UserProfile>();
+
+    public UserProfile addUser(String login, String email, String password) {
+        final UserProfile newUserProfile = new UserProfile(login, email, password);
+        loginToUser.put(login, newUserProfile);
+        return  newUserProfile;
+    }
+
+    public UserProfile getUserByLogin(String login) {
+        return loginToUser.get(login);
+    }
+}
