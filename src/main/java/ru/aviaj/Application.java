56a69604a1292ce32caea623c91ae08510f99beb
TimeEscaper <rentgeny05@gmail.com>
2016-10-02 22:39:17 +0300

    Исправлена поддержка CORS.

--- src/main/java/ru/aviaj/Application.java
+++ src/main/java/ru/aviaj/Application.java
@@ -22,7 +22,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
