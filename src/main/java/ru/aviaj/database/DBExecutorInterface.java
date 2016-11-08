f0ccc23cc70923ccfc75ae90c28a991944c443eb
TimeEscaper <rentgeny05@gmail.com>
2016-11-08 09:23:40 +0300

    Начата архитектура работы с БД.

--- src/main/java/ru/aviaj/database/DBExecutorInterface.java
+++
@@ -1,5 +0,0 @@
-package ru.aviaj.database;
-
-public interface DBExecutorInterface {
-
-}
--- src/main/java/ru/aviaj/database/DBServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package ru.aviaj.database;
-
-/**
- * Created by sibirsky on 06.11.16.
- */
-public class DBServiceInterface {
-    DBExecutorInterface executor;
-}
