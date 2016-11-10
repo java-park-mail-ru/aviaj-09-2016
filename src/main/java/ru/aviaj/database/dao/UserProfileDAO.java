e3174499be1ecc92d7bfec75633dd00e7622b1c9
TimeEscaper <rentgeny05@gmail.com>
2016-11-11 02:38:41 +0300

    Реализован класс UserProfileDAO, изменён UserProfile.

--- src/main/java/ru/aviaj/database/dao/UserProfileDAO.java
+++ src/main/java/ru/aviaj/database/dao/UserProfileDAO.java
@@ -1,10 +1,14 @@
 package ru.aviaj.database.dao;
 
+import org.springframework.security.access.method.P;
 import ru.aviaj.database.executor.Executor;
 import ru.aviaj.model.UserProfile;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class UserProfileDAO {
 
@@ -28,9 +32,112 @@ public class UserProfileDAO {
                         resultSet.getLong("rating"));
             });
         } catch (SQLException e) {
+            System.out.println(Integer.toString(e.getErrorCode()) + ": " + e.getSQLState());
             return null;
         }
 
         return user;
+    }
+
+    public UserProfile getUserByLogin(String login) {
+        Executor executor = new Executor();
+        String query = "SELECT * FROM User WHERE login = " + login + ";";
+        UserProfile user;
+        try {
+            user = executor.execQuery(dbConnection, query, resultSet -> {
+                resultSet.next();
+                return new UserProfile(resultSet.getString("login"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        resultSet.getLong("id"),
+                        resultSet.getLong("rating"));
+            });
+        } catch (SQLException e) {
+            System.out.println(Integer.toString(e.getErrorCode()) + ": " + e.getSQLState());
+            return null;
+        }
+
+        return user;
+    }
+
+    public List<UserProfile> getTopUsers() {
+        Executor executor = new Executor();
+        String query = "SELECT * FROM User ORDER BY rating DESC LIMIT 10;";
+        List<UserProfile> userList = new ArrayList<>();
+        try {
+                Collections.copy(userList, executor.execQuery(dbConnection, query, resultSet -> {
+                List<UserProfile> bufferUserList = new ArrayList<>();
+                while (resultSet.next()) {
+                    bufferUserList.add(new UserProfile(resultSet.getString("login"),
+                            resultSet.getString("email"),
+                            resultSet.getString("password"),
+                            resultSet.getLong("id"),
+                            resultSet.getLong("rating")));
+                }
+
+                return bufferUserList;
+            }));
+        } catch (SQLException e) {
+            System.out.println(Integer.toString(e.getErrorCode()) + ": " + e.getSQLState());
+            return null;
+        }
+
+        return userList;
+    }
+
+    public List<UserProfile> getUsers() {
+        Executor executor = new Executor();
+        String query = "SELECT * FROM User;";
+        List<UserProfile> userList = new ArrayList<>();
+        try {
+            Collections.copy(userList, executor.execQuery(dbConnection, query, resultSet -> {
+                List<UserProfile> bufferUserList = new ArrayList<>();
+                while (resultSet.next()) {
+                    bufferUserList.add(new UserProfile(resultSet.getString("login"),
+                            resultSet.getString("email"),
+                            resultSet.getString("password"),
+                            resultSet.getLong("id"),
+                            resultSet.getLong("rating")));
+                }
+
+                return bufferUserList;
+            }));
+        } catch (SQLException e) {
+            System.out.println(Integer.toString(e.getErrorCode()) + ": " + e.getSQLState());
+            return null;
+        }
+
+        return userList;
+    }
+
+    public UserProfile addUser(String login, String email, String password) {
+        Executor executor = new Executor();
+        String update = "INSERT INTO User(login, email, password) VALUES('" +
+                login + "','" + email + "','" + password + "');";
+        try {
+            executor.execUpdate(dbConnection, update);
+        }
+        catch (SQLException e) {
+            System.out.println(Integer.toString(e.getErrorCode()) + ": " + e.getSQLState());
+            return  null;
+        }
+        UserProfile user = getUserByLogin(login);
+
+        return user;
+    }
+
+    public boolean updateRating(long id, long incValue) {
+        Executor executor = new Executor();
+        String update = "UPDATE User set rating = (rating + " + Long.toString(incValue) + ") WHERE id = " +
+                Long.toString(id) + ";";
+        try {
+            executor.execUpdate(dbConnection, update);
+        }
+        catch (SQLException e) {
+            System.out.println(Integer.toString(e.getErrorCode()) + ": " + e.getSQLState());
+            return false;
+        }
+
+        return true;
     }
 }
--- src/main/java/ru/aviaj/model/UserProfile.java
+++ src/main/java/ru/aviaj/model/UserProfile.java
@@ -4,7 +4,7 @@ package ru.aviaj.model;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SuppressWarnings({"ConstantNamingConvention", "unused"})
-public class UserProfile {
+public class UserProfile implements Cloneable{
 
     private long id;
     private String login;
@@ -22,12 +22,20 @@ public class UserProfile {
     }
 
     public UserProfile(String login, String email, String password, long id, long rating) {
-
         this.id = id;
         this.login = login;
         this.email = email;
         this.password = password;
         this.rating = rating;
+    }
+
+    public UserProfile(UserProfile other) {
+
+        this.id = other.getId();
+        this.login = other.getLogin();
+        this.email = other.getEmail();
+        this.password = other.getPassword();
+        this.rating = other.getRating();
     }
 
     public long getId() {
