71a0c1644259466aa7372fdbbbc7a9cc18e438d3
TimeEscaper <rentgeny05@gmail.com>
2016-11-21 23:15:47 +0300

    Несколько исправлений

--- src/main/java/ru/aviaj/database/handler/IResultHandler.java
+++ src/main/java/ru/aviaj/database/handler/IResultHandler.java
@@ -3,7 +3,7 @@ package ru.aviaj.database.handler;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
+@FunctionalInterface
 public interface IResultHandler<T> {
     T handle(ResultSet resultSet) throws SQLException;
 }
