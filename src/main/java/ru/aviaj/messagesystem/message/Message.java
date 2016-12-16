918e1a9529ea7899eed301e0f9ea808443fb03e9
TimeEscaper <rentgeny05@gmail.com>
2016-12-17 02:12:24 +0300

    Добавлен класс Message.

---
+++ src/main/java/ru/aviaj/messagesystem/message/Message.java
@@ -0,0 +1,25 @@
+package ru.aviaj.messagesystem.message;
+
+import ru.aviaj.messagesystem.Abonent;
+import ru.aviaj.messagesystem.Address;
+
+public abstract class Message {
+
+    private final Address from;
+    private final Address to;
+
+    public Message(Address from, Address to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Address getFrom() {
+        return from;
+    }
+
+    public Address getTo() {
+        return to;
+    }
+
+    public abstract void exec(Abonent abonent);
+}
