24d29981a7f02f2b051411849e3387484590dc2e
TimeEscaper <rentgeny05@gmail.com>
2016-12-17 10:44:33 +0300

    Добавлен класс MessageSystem.

--- src/main/java/ru/aviaj/messagesystem/MessageSystem.java
+++ src/main/java/ru/aviaj/messagesystem/MessageSystem.java
@@ -1,4 +1,26 @@
 package ru.aviaj.messagesystem;
 
+import ru.aviaj.messagesystem.message.Message;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class MessageSystem {
+
+    private final Map<Address, ConcurrentLinkedQueue<Message>> messages = new HashMap<>();
+
+    public void send(Message message) {
+        final Queue<Message> msgQueue = messages.get(message.getTo());
+        msgQueue.add(message);
+    }
+
+    public void execForAbonent(Abonent abonent) {
+        final Queue<Message> msgQueue = messages.get(abonent.getAddress());
+        while (!msgQueue.isEmpty()) {
+            final Message msg = msgQueue.poll();
+            msg.exec(abonent);
+        }
+    }
 }
