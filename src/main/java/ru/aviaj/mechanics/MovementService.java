eed98c6aa0614020722e679707d5b50d90c6b5ab
TimeEscaper <rentgeny05@gmail.com>
2016-12-25 16:15:49 +0300

    Добавлено движение.

--- src/main/java/ru/aviaj/mechanics/MovementService.java
+++ src/main/java/ru/aviaj/mechanics/MovementService.java
@@ -1,5 +1,7 @@
 package ru.aviaj.mechanics;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.aviaj.mechanics.baseobject.PlanePosition;
 import ru.aviaj.mechanics.baseobject.Ring;
 import ru.aviaj.mechanics.baseobject.TrackMap;
@@ -7,19 +9,24 @@ import ru.aviaj.mechanics.basetype.CollisionStatus;
 import ru.aviaj.mechanics.basetype.Dot;
 import ru.aviaj.mechanics.basetype.DotDouble;
 import ru.aviaj.mechanics.gamesession.GameSession;
+import ru.aviaj.mechanics.gamesession.GameSessionService;
 import ru.aviaj.mechanics.geometry.Geometry;
 
-import java.util.HashSet;
-import java.util.Set;
-
+@SuppressWarnings({"unused", "OverlyComplexBooleanExpression", "OverlyComplexMethod"})
+@Service
 public class MovementService {
 
-    final Set<GameSession> gameSessions = new HashSet<>();
+    GameSessionService gameSessionService;
+
+    @Autowired
+    public MovementService(GameSessionService gameSessionService) {
+        this.gameSessionService = gameSessionService;
+    }
 
     private CollisionStatus processRingCollision(PlanePosition position, Ring ring) {
-        DotDouble projection = Geometry.dotPlaneProjection(position.getCenter(), ring.getNormal());
+        final DotDouble projection = Geometry.dotPlaneProjection(position.getCenter(), ring.getNormal());
 
-        Dot nextPosition = new Dot(position.getCenter().getX() + position.getSpeedDirection().getX(),
+        final Dot nextPosition = new Dot(position.getCenter().getX() + position.getSpeedDirection().getX(),
                 position.getCenter().getY() + position.getSpeedDirection().getY(),
                 position.getCenter().getZ() + position.getSpeedDirection().getZ());
 
@@ -35,25 +42,64 @@ public class MovementService {
         return CollisionStatus.NONE;
     }
 
-    public void addSession(GameSession gameSession) {
-        gameSessions.add(gameSession);
+    private CollisionStatus processCollisions(GameSession gameSession) {
+
+        final TrackMap track = gameSession.getTrack();
+
+        final PlanePosition planePositionFirst = gameSession.getPlayerFirst().getPlanePosition();
+
+        for(Ring ring : track.getRings()) {
+            if (processRingCollision(planePositionFirst, ring) == CollisionStatus.RING_OVERFLY) {
+                gameSession.updateRatingFirst(ring.getRatingValue());
+            }
+        }
+
+        final PlanePosition planePositionSecond = gameSession.getPlayerSecond().getPlanePosition();
+
+        for(Ring ring : track.getRings()) {
+            if (processRingCollision(planePositionSecond, ring) == CollisionStatus.RING_OVERFLY) {
+                gameSession.updateRatingSecond(ring.getRatingValue());
+            }
+        }
+
+        if ((planePositionFirst.getCenter().getX() > GameConfig.TRACK_WIDTH) ||
+                (planePositionFirst.getCenter().getY() > GameConfig.TRACK_WIDTH)) {
+            return CollisionStatus.OUT_OF_TRACK;
+        }
+        if ((planePositionSecond.getCenter().getX() > GameConfig.TRACK_WIDTH) ||
+                (planePositionSecond.getCenter().getY() > GameConfig.TRACK_WIDTH)) {
+            return CollisionStatus.OUT_OF_TRACK;
+        }
+
+        if (planePositionFirst.getCenter().getZ() < 5) {
+            return CollisionStatus.GROUND;
+        }
+        if (planePositionSecond.getCenter().getZ() < 5) {
+            return CollisionStatus.GROUND;
+        }
+
+        return CollisionStatus.OK;
     }
 
-    public void processMovements() {
-        for(GameSession gameSession : gameSessions) {
+    public void processMovements(GameSession gameSession) {
 
-            TrackMap track = gameSession.getTrack();
+        final PlanePosition planePositionFirst = gameSession.getPlayerFirst().getPlanePosition();
+        final PlanePosition newFirstPosition = new PlanePosition(
+                new Dot(planePositionFirst.getCenter().getX() + planePositionFirst.getSpeedDirection().getX(),
+                        planePositionFirst.getCenter().getY() + planePositionFirst.getSpeedDirection().getY(),
+                        planePositionFirst.getCenter().getZ() + planePositionFirst.getSpeedDirection().getZ()),
+                planePositionFirst.getSpeedDirection()
+        );
+        gameSession.getPlayerFirst().setPlanePosition(newFirstPosition);
 
-            PlanePosition planePosition = gameSession.getPlayerFirst().getPlanePosition();
-
-            for(Ring ring : track.getRings()) {
-                if (processRingCollision(planePosition, ring) == CollisionStatus.RING_OVERFLY) {
-                    gameSession.updateRatingFirst(ring.getRatingValue());
-                }
-            }
-
-
-        }
+        final PlanePosition planePositionSecond = gameSession.getPlayerSecond().getPlanePosition();
+        final PlanePosition newSecondPosition = new PlanePosition(
+                new Dot(planePositionSecond.getCenter().getX() + planePositionSecond.getSpeedDirection().getX(),
+                        planePositionSecond.getCenter().getY() + planePositionSecond.getSpeedDirection().getY(),
+                        planePositionSecond.getCenter().getZ() + planePositionSecond.getSpeedDirection().getZ()),
+                planePositionSecond.getSpeedDirection()
+        );
+        gameSession.getPlayerSecond().setPlanePosition(newFirstPosition);
     }
 
 }
--- src/main/java/ru/aviaj/mechanics/basetype/CollisionStatus.java
+++ src/main/java/ru/aviaj/mechanics/basetype/CollisionStatus.java
@@ -3,6 +3,7 @@ package ru.aviaj.mechanics.basetype;
 @SuppressWarnings("unused")
 public enum CollisionStatus {
     NONE,
+    OK,
     RING_OVERFLY,
     RING_COLLISION,
     GROUND,
