44e3c9b65d3e0ab6281d961531de86d00d5a8c48
TimeEscaper <rentgeny05@gmail.com>
2017-01-03 20:18:35 +0300

    Добавлен класс MinimaluserProfile.

---
+++ src/main/java/ru/aviaj/mechanics/baseobject/MinimalUserProfile.java
@@ -0,0 +1,27 @@
+package ru.aviaj.mechanics.baseobject;
+
+import ru.aviaj.model.UserProfile;
+
+public class MinimalUserProfile {
+
+    private long id;
+    private String login;
+
+    public MinimalUserProfile(UserProfile userProfile) {
+        this.id = userProfile.getId();
+        this.login = userProfile.getLogin();
+    }
+
+    public MinimalUserProfile(long id, String login) {
+        this.id = id;
+        this.login = login;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
